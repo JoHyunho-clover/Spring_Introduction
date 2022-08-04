86e8767ade2341a1a733d09f0b13c2b6c911f0cd
JoHyunho-clover <chlrh330@naver.com>
2022-08-04 21:52:21 +0900

    웹 MVC개발 회원 등록 & 회원 목록 조회

--- src/main/java/introduction/springintroduction/controller/MemberController.java
+++ src/main/java/introduction/springintroduction/controller/MemberController.java
@@ -1,8 +1,14 @@
 package introduction.springintroduction.controller;
 
+import introduction.springintroduction.domain.Member;
 import introduction.springintroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller//스프링 컨테이너에 스프링 빈으로써 등록해주는 어노테이션
 public class MemberController {
@@ -22,5 +28,26 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> mebers=memberService.findMembers();
+        model.addAttribute("members",mebers);
+        return "members/memberList";
     }
 }
